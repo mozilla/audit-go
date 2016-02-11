154cc0d35a19f78da91ca79ca03e4068c9d2e52e
Hardik Juneja <hardikjuneja.hj@gmail.com>
2016-02-11 06:40:57 +0530

    add some new file rules cases and modify rules2josn.py accordingly

--- tools/rules2json.py
+++ tools/rules2json.py
@@ -13,21 +13,35 @@ with open(sys.argv[1], 'r') as my_file:
 			rule = rule.split()
 			if rule[0] == "-w":
 				# parse watches on file system
-				if len(rule) > 4 and rule[2] == "-p" and rule[4] == "-k":
-					perm = rule[3]
-					kname = rule[5]
-				elif len(rule) > 4 and rule[2] == "-k" and rule[4] == "-p":
-					perm = rule[5]
-					kname = rule[3]
-				elif len(rule) == 4 and rule[2] == "-p":
-					perm = rule[3]
+				json = {'path':rule[1]}
+
+				if len(rule) >= 2:
+					if rule[2] == "-p":
+						if len(rule) > 4 and rule[4] == "-k":
+							json['permission'] = rule[3]
+							json['key'] = rule[5] 
+						else:
+							json['permission'] = rule[3]
+					elif rule[2] == "-k": 
+						if len(rule) > 4 and rule[4] == "-p":
+							json['permission'] = rule[5]
+							json['key'] = rule[3] 
+						else:
+							json['key'] = rule[3]
 				else:
 					print("Invalid rule: "+" ".join(rule))
 					exit()
-				watches.append({'path':rule[1], 'permission': perm, 'key': kname})
+				watches.append(json)
 			elif rule[0] == "-a":
 				# parse syscalls
+				# TODO: take note of -a and -A
 				print(rule)
-			else:
-				print(rule)
-	#print watches
+			#else:
+				#print(rule)
+	print watches
+# -w /etc/syslog.conf
+# -w /etc/syslog-ng.conf -p wa -k syslog
+# -w /etc/syslog.conf -p wa 
+# -w /etc/rsyslog.conf -k syslog -p wa
+# -w /etc/rsyslog-ng/ -k syslog
+
